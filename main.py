ffbd375085e3ecd2475bd181590624951242e2e9
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2020-02-24 01:19:05 -0300

    initial: iniciando o desenvolvimento da aplicação
    Script funcional, já consegue enviar e-mails sem apresenta problemas.

---
+++ main.py
@@ -0,0 +1,47 @@
+'''
+    Enviando e-mail com python
+    Material de apoio
+    https://blog.chcdc.com.br/posts/enviar-emails-com-python/
+    https://code.tutsplus.com/pt/tutorials/sending-emails-in-python-with-smtp--cms-29975
+    https://www.pythonforbeginners.com/code-snippets-source-code/using-python-to-send-email
+    http://rosettacode.org/wiki/Send_email#Python
+'''
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+# mensagem qeu será enviada
+message = '''
+
+Desenvolvido por Jamerson Walderson
+
+'''
+# cria a instancia para a mensagem
+msg = MIMEMultipart()
+# seu e-mail e senha
+my_email = ""
+my_password = ""
+from_email = my_email
+
+# separar da seguinte forma ['','','']
+# caso queira fazer um teste existem sites como o 10MinuteMail
+to_email = ['']
+# titulo da mensagem
+msg['Subject'] = "Enviada atrávez de um script"
+
+msg.attach(MIMEText(message, 'plain'))
+# conectando-se aos servidores do Gmail e protegendo login e senha
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(from_email, my_password)
+
+# dispara o e-mail
+texto = msg.as_string()
+try:
+    for send in to_email:
+        server.sendmail(from_email, send, texto)
+        print ("Enviado com sucesso para ", send)
+except:
+    print ("Erro ao enviar e-mail para ", send)
+finally:
+    server.quit()
